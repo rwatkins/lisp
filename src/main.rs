dafee83a58a4cf2a5c4abe5f0f5347e876b0435b
Riley Watkins <riley@rileywatkins.com>
2018-04-10 19:53:52 -0700

    Create lex module

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,13 @@
 extern crate itertools;
 extern crate regex;
 
+mod lex;
+use lex::*;
+
 use std::collections::VecDeque;
 use std::fmt;
 use std::io::{self, Read};
-use std::str::FromStr;
 use itertools::Itertools;
-use regex::Regex;
-
-#[derive(Debug, PartialEq, Clone)]
-enum Token {
-    LParen,
-    RParen,
-    LBracket,
-    RBracket,
-    Symbol(String),
-    Number(i32),
-    Whitespace(String),
-}
-
-type LexResult = Option<(Token, usize)>;
 
 // data LispVal = Atom String
 //              | List [LispVal]
@@ -53,110 +41,6 @@ impl fmt::Display for LispVal {
             Nil => write!(f, "nil"),
         }
     }
-}
-
-fn lex_whitespace(s: &str, i: usize) -> Option<(Token, usize)> {
-    let re = Regex::new(r"(^\s+)").unwrap();
-    let caps = re.captures(&s[i..])?;
-    let whitespace = caps.get(1)?.as_str();
-    Some((Token::Whitespace(whitespace.to_string()), i + whitespace.len()))
-}
-
-fn lex_lparen(s: &str, i: usize) -> Option<(Token, usize)> {
-    lex_char(&s, i, '(', Token::LParen)
-}
-
-fn lex_lbracket(s: &str, i: usize) -> Option<(Token, usize)> {
-    lex_char(&s, i, '[', Token::LBracket)
-}
-
-fn lex_rbracket(s: &str, i: usize) -> LexResult {
-    lex_char(&s, i, ']', Token::RBracket)
-}
-
-fn lex_rparen(s: &str, i: usize) -> Option<(Token, usize)> {
-    lex_char(s, i, ')', Token::RParen)
-}
-
-fn lex_char(s: &str, i: usize, c: char, token: Token) -> Option<(Token, usize)> {
-    if s[i..].starts_with(c) {
-        Some((token, i + 1))
-    } else {
-        None
-    }
-}
-
-fn lex_symbol(s: &str, i: usize) -> Option<(Token, usize)> {
-    let re = Regex::new(r"^([\-_+a-zA-Z\*/]+[a-zA-Z0-9]*)").unwrap();
-    let caps = re.captures(&s[i..])?;
-    let symbol = caps.get(1)?.as_str();
-    Some((Token::Symbol(symbol.to_string()), i + symbol.len()))
-}
-
-#[test]
-fn test_word_is_a_valid_symbol() {
-    assert_eq!(lex_symbol("asdf", 0), Some((Token::Symbol("asdf".into()), 4)));
-}
-
-#[test]
-fn test_symbol_cannot_start_with_a_number() {
-    assert_eq!(lex_symbol("9asdf", 0), None);
-}
-
-#[test]
-fn test_lex_symbol_respects_index() {
-    assert_eq!(lex_symbol("9 asdf", 2), Some((Token::Symbol("asdf".into()), 6)));
-}
-
-#[test]
-fn test_lex_plus() {
-    assert_eq!(lex_symbol("+", 0), Some((Token::Symbol("+".into()), 1)));
-}
-
-#[test]
-fn test_lex_minus() {
-    assert_eq!(lex_symbol("-", 0), Some((Token::Symbol("-".into()), 1)));
-}
-
-fn lex_number(s: &str, i: usize) -> Option<(Token, usize)> {
-    let re = Regex::new(r"^(\d+)").unwrap();
-    let caps = re.captures(&s[i..])?;
-    let number = caps.get(1)?.as_str();
-    Some((Token::Number(i32::from_str(number).unwrap()), i + number.len()))
-}
-
-fn lex_once(s: &str, index: usize) -> Option<(Token, usize)> {
-    let token_fns = [
-        lex_whitespace,
-        lex_lparen,
-        lex_rparen,
-        lex_lbracket,
-        lex_rbracket,
-        lex_number,
-        lex_symbol,
-    ];
-
-    for lex_fn in token_fns.iter() {
-        if let Some((t, i)) = lex_fn(s, index) {
-            return Some((t, i));
-        }
-    }
-
-    None
-}
-
-fn lex(s: &str) -> Result<Vec<Token>, String> {
-    let mut tokens = vec![];
-    let mut index = 0;
-    while index < s.len() {
-        if let Some((t, i)) = lex_once(s, index) {
-            tokens.push(t);
-            index = i;
-        } else {
-            return Err(format!("Unexpected token: {}", s[index..index+1].to_string()));
-        }
-    }
-    Ok(tokens)
 }
 
 fn parse_list(tokens: &mut VecDeque<Token>) -> Result<LispVal, String> {
