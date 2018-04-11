ecf5a1cfeed877e9ff60af29739dc2f60ee44a52
Riley Watkins <riley@rileywatkins.com>
2018-04-11 08:35:33 -0700

    Split up call_function into separate functions

--- src/eval.rs
+++ src/eval.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 use parse::LispVal;
 
+type EvalResult = Result<LispVal, String>;
 type Scope = HashMap<String, LispVal>;
 
 fn is_truthy(val: &LispVal) -> bool {
@@ -10,92 +11,104 @@ fn is_truthy(val: &LispVal) -> bool {
     }
 }
 
-fn call_function(f: &LispVal, args: &Vec<LispVal>, scope: &Scope) -> Result<LispVal, String> {
+fn call_add(args: &Vec<LispVal>, _: &Scope) -> EvalResult {
+    for arg in args {
+        if !is_truthy(arg) {
+            return Ok(arg.clone());
+        }
+    }
+    Ok(args[args.len() - 1].clone())
+}
+
+fn call_or(args: &Vec<LispVal>, _: &Scope) -> EvalResult {
+    for arg in args {
+        if is_truthy(arg) {
+            return Ok(arg.clone());
+        }
+    }
+    Ok(args[args.len() - 1].clone())
+}
+
+fn call_plus(args: &Vec<LispVal>, scope: &Scope) -> EvalResult {
+    let mut plus_args: Vec<i32> = vec![];
+    for arg in args {
+        match arg {
+            &LispVal::Number(i) => plus_args.push(i),
+            lst@&LispVal::List(..) => {
+                let result = eval(lst.clone(), &scope)?;
+                match result {
+                    LispVal::Number(i) => plus_args.push(i),
+                    v => return Err(format!("Unexpected arg to plus 1: {:?}", v)),
+                }
+            }
+            s@&LispVal::Symbol(..) => {
+                match eval(s.clone(), &scope)? {
+                    LispVal::Number(i) => plus_args.push(i),
+                    v => return Err(format!("Unexpected arg to plus 2: {:?}", v)),
+                }
+
+            }
+            v => return Err(format!("Unexpected arg to plus 3: {:?}", v)),
+        }
+    }
+    Ok(LispVal::Number(plus_args.iter().fold(0, |acc, i| acc + i)))
+}
+
+fn call_minus(args: &Vec<LispVal>, scope: &Scope) -> EvalResult {
+    let mut result = match eval(args[0].clone(), &scope)? {
+        LispVal::Number(i) => i,
+        x => return Err(format!("Unexpected arg to minus: {:?}", x)),
+    };
+    for arg in args[1..].iter() {
+        match eval(arg.clone(), &scope)? {
+            LispVal::Number(ref i) => result -= i,
+            x => return Err(format!("Unexpected arg to minus: {:?}", x)),
+        }
+    }
+    Ok(LispVal::Number(result))
+}
+
+fn call_mult(args: &Vec<LispVal>, scope: &Scope) -> EvalResult {
+    let mut result = match eval(args[0].clone(), &scope)? {
+        LispVal::Number(i) => i,
+        x => return Err(format!("Unexpected arg to mult: {:?}", x)),
+    };
+    for arg in args[1..].iter() {
+        match eval(arg.clone(), &scope)? {
+            LispVal::Number(ref i) => result *= i,
+            x => return Err(format!("Unexpected arg to mult: {:?}", x)),
+        }
+    }
+    Ok(LispVal::Number(result))
+}
+
+fn call_div(args: &Vec<LispVal>, scope: &Scope) -> EvalResult {
+    let mut result = match eval(args[0].clone(), &scope)? {
+        LispVal::Number(i) => i,
+        x => return Err(format!("Unexpected arg to div: {:?}", x)),
+    };
+    for arg in args[1..].iter() {
+        match eval(arg.clone(), &scope)? {
+            LispVal::Number(ref i) => result /= i,
+            x => return Err(format!("Unexpected arg to div: {:?}", x)),
+        }
+    }
+    Ok(LispVal::Number(result))
+}
+
+fn call_function(f: &LispVal, args: &Vec<LispVal>, scope: &Scope) -> EvalResult {
     let fn_name = match f {
         &LispVal::Function(ref s) => s,
         _ => return Err(format!("Don't know how to do that yet (call_function: {:?})", f)),
     };
 
     match fn_name.as_ref() {
-        "and" => {
-            for arg in args {
-                if !is_truthy(arg) {
-                    return Ok(arg.clone());
-                }
-            }
-            Ok(args[args.len() - 1].clone())
-        }
-        "or" => {
-            for arg in args {
-                if is_truthy(arg) {
-                    return Ok(arg.clone());
-                }
-            }
-            Ok(args[args.len() - 1].clone())
-        }
-        "+" => {
-            let mut plus_args: Vec<i32> = vec![];
-            for arg in args {
-                match arg {
-                    &LispVal::Number(i) => plus_args.push(i),
-                    lst@&LispVal::List(..) => {
-                        let result = eval(lst.clone(), &scope)?;
-                        match result {
-                            LispVal::Number(i) => plus_args.push(i),
-                            v => return Err(format!("Unexpected arg to plus 1: {:?}", v)),
-                        }
-                    }
-                    s@&LispVal::Symbol(..) => {
-                        match eval(s.clone(), &scope)? {
-                            LispVal::Number(i) => plus_args.push(i),
-                            v => return Err(format!("Unexpected arg to plus 2: {:?}", v)),
-                        }
-
-                    }
-                    v => return Err(format!("Unexpected arg to plus 3: {:?}", v)),
-                }
-            }
-            Ok(LispVal::Number(plus_args.iter().fold(0, |acc, i| acc + i)))
-        }
-        "-" => {
-            let mut result = match eval(args[0].clone(), &scope)? {
-                LispVal::Number(i) => i,
-                x => return Err(format!("Unexpected arg to minus: {:?}", x)),
-            };
-            for arg in args[1..].iter() {
-                match eval(arg.clone(), &scope)? {
-                    LispVal::Number(ref i) => result -= i,
-                    x => return Err(format!("Unexpected arg to minus: {:?}", x)),
-                }
-            }
-            Ok(LispVal::Number(result))
-        }
-        "*" => {
-            let mut result = match eval(args[0].clone(), &scope)? {
-                LispVal::Number(i) => i,
-                x => return Err(format!("Unexpected arg to mult: {:?}", x)),
-            };
-            for arg in args[1..].iter() {
-                match eval(arg.clone(), &scope)? {
-                    LispVal::Number(ref i) => result *= i,
-                    x => return Err(format!("Unexpected arg to mult: {:?}", x)),
-                }
-            }
-            Ok(LispVal::Number(result))
-        }
-        "/" => {
-            let mut result = match eval(args[0].clone(), &scope)? {
-                LispVal::Number(i) => i,
-                x => return Err(format!("Unexpected arg to div: {:?}", x)),
-            };
-            for arg in args[1..].iter() {
-                match eval(arg.clone(), &scope)? {
-                    LispVal::Number(ref i) => result /= i,
-                    x => return Err(format!("Unexpected arg to div: {:?}", x)),
-                }
-            }
-            Ok(LispVal::Number(result))
-        }
+        "and" => call_add(&args, &scope),
+        "or" => call_or(&args, &scope),
+        "+" => call_plus(&args, &scope),
+        "-" => call_minus(&args, &scope),
+        "*" => call_mult(&args, &scope),
+        "/" => call_div(&args, &scope),
         s => Err(format!("Don't know how to do that yet (call_function: {})", s)),
     }
 }
